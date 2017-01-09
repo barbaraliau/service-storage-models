cbadb6e9830681ea36689077f86c63a3167c1726
Barbara Liau <bj.liau@gmail.com>
2017-01-09 14:57:50 -0700

    Add before/after connections

--- test/payment-processor-adapters/stripe.unit.js
+++ test/payment-processor-adapters/stripe.unit.js
@@ -2,51 +2,71 @@
 
 /*jshint expr: true*/
 
+const mongoose = require('mongoose');
 const chai = require('chai');
 const expect = chai.expect;
 const stripeAdapter =
   require('../../lib/models/payment-processor-adapters/stripe');
+const PaymentProcessorSchema = require('../../lib/models/payment-processor');
 
-  describe('Storage/models/payment-processor-adapters/stripeAdapter',
-  function() {
+var PaymentProcessor;
+var connection;
 
-    describe('#serializeData', function() {
+before(function(done) {
+  connection = mongoose.createConnection(
+    'mongodb://127.0.0.1:27017__storj-bridge-test',
+    function() {
+      PaymentProcessor = PaymentProcessorSchema(connection);
+      done();
+    }
+  );
+})
 
-      it('should serialize rawData', function(done) {
-
-      });
-
-    });
-
-    describe('#parseData', function() {
-
-      it('should parse rawData', function(done) {
-
-      });
-
-    });
-
-    describe('#register', function() {
-
-      it('should use the stripe api to create customer', function(done) {
-
-      });
-
-      it('should fail without credit card token', function(done) {
-
-      });
-
-      it('should fail without user email', function(done) {
-
-      });
-
-    });
-
-    describe('#delete', function() {
-
-      it('should delete stripe customer', function(done) {
-
-      });
-    });
-
+after(function(done) {
+  PaymentProcessor.remove({}, function() {
+    connection.close(done);
   });
+});
+
+// describe('Storage/models/payment-processor-adapters/stripeAdapter', function() {
+//
+//   describe('#serializeData', function() {
+//
+//     it('should serialize rawData', function(done) {
+//       stripeAdapter.add()
+//     });
+//
+//   });
+//
+//   describe('#parseData', function() {
+//
+//     it('should parse rawData', function(done) {
+//
+//     });
+//
+//   });
+//
+//   describe('#register', function() {
+//
+//     it('should use the stripe api to create customer', function(done) {
+//
+//     });
+//
+//     it('should fail without credit card token', function(done) {
+//
+//     });
+//
+//     it('should fail without user email', function(done) {
+//
+//     });
+//
+//   });
+//
+//   describe('#delete', function() {
+//
+//     it('should delete stripe customer', function(done) {
+//
+//     });
+//   });
+//
+// });
